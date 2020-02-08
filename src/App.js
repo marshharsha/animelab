a0fc615f76eb927c144066176cca95774f0d2b0a
animelab <animelab>
2020-02-08 00:26:01 -0800

    w

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import dashBoard from './animeComponents/dashBoard'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <dashBoard/>  
+      </div>
+    )
+  }
+}
